1a65b080285aba0678a8c2db6c4e6772e8700504
dez_chuang <Dez_Chuang@compal.com>
2017-04-05 11:49:32 +0800

    Add mlab db and start script

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes   = require("./routes/comments"),
     postRoutes      = require("./routes/posts"),
     indexRoutes     = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/dez_blog");
+//mongoose.connect("mongodb://localhost/dez_blog");
+mongoose.connect("mongodb://dezchuang:qqqqq12345@ds147777.mlab.com:47777/deztwblog");
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
